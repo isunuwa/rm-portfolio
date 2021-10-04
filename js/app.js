9b8643fa8390240560fabd05bfa3aeae01b0dbe7
israel sunuwar <isunuwar@inctic.io>
2021-10-04 18:56:49 +0545

    integrated responsive navbar

--- js/app.js
+++ js/app.js
@@ -15,4 +15,42 @@ icon.addEventListener('click', function(){
   }
 });
 
-// fa-circle
+// for mbl mblIcon
+let  mblIcon = document.querySelector(".mbl-moon");
+let  mblLogo = document.querySelector(".mbl-rz-logo");
+
+mblIcon.addEventListener('click', function(){
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    mblLogo.src = "./assets/images/logo-light.png";
+    mblIcon.className = 'fa fa-circle';
+  }
+  else{
+    mblLogo.src = "./assets/images/logo.png";
+    mblIcon.className = 'fa fa-moon-o';
+  }
+});
+
+// responsive navigation
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.rz-navbar-wrapper');
+let navLinks = document.querySelectorAll('.rz-navbar-wrapper ul li');
+
+const toggleNav = function() {
+  nav.classList.toggle('active');
+  document.body.classList.toggle("fixed-position");
+  // burger animation
+  burger.classList.toggle('active');
+}
+
+const navSlide = () => {
+  burger.addEventListener('click', toggleNav);
+}
+
+const navClick = () => {
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', toggleNav);
+  });
+}
+
+burger.addEventListener('click', navClick(), navSlide());
